2e7cf2396f3c8cc9dd88f00975cccf8286a54e91
Daniel <gacenko.daniil@gmail.com>
2025-10-05 16:11:44 +0300

    s

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { StoreProvider } from "@/app/providers/StoreProvider";
 import { AppRouter } from "@/app/providers/Router";
+import "@/app/styles/index.scss";
 
 export default function App() {
   return (
